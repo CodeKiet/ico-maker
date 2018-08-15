41246264c69c09b8d1ca3a4642e705b0fb6d8d04
vittominacori <vittoriominacori@gmail.com>
2018-08-15 20:43:25 +0200

    updated openzeppelin

--- contracts/crowdsale/utils/Contributions.sol
+++ contracts/crowdsale/utils/Contributions.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/ownership/rbac/RBAC.sol";
+import "openzeppelin-solidity/contracts/access/rbac/RBAC.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 
