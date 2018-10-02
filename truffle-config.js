c38becd93799a1fb52e92d907c8311449af6d2da
vittominacori <vittoriominacori@gmail.com>
2018-10-02 23:31:45 +0200

    fix truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   networks: {
-    development: {
+    develop: {
       host: '127.0.0.1',
       port: 9545,
       network_id: '*', // eslint-disable-line camelcase
